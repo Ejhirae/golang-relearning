23777c26902db191eab62938a3037ec8a1258b3e
ejhirae <victor.osahon24@gmail.com>
2023-12-16 13:24:15 +0000

    updated variables.go

--- variables/main.go
+++ variables/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 )
 
@@ -10,14 +11,14 @@ import (
 // getting warmer or colder
 // Checks if the difference between guessed number and actual number is 25
 func main() {
-	guessingNumber := 24
+	guessingNumber := rand.Intn(98) + 2
 	numberOfGuesses := 3
 	var userInput int
 	fmt.Println("Guess a Number between 1 - 100")
 	fmt.Scan(&userInput)
 
 	for userInput != guessingNumber {
-		if numberOfGuesses > 0 && math.Abs(float64(userInput-guessingNumber)) <= 25 {
+		if numberOfGuesses > 1 && math.Abs(float64(userInput-guessingNumber)) <= 25 {
 			fmt.Println("Keep guessing you are getting warmer ")
 			numberOfGuesses--
 			fmt.Println("number of guesses: ", numberOfGuesses)
